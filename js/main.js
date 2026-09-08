3f8eb98e2d2f431e1fe8ed2effaeb6c26e00c646
André-Marie <andremariebe128@gmail.com>
2026-09-08 14:29:06 +0100

    test

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
    (desktop + mobile) sur toutes les pages. Repassez à false pour
    le masquer à nouveau une fois l'événement terminé.
    ========================================================== */
-var EVENTS_ACTIVE = false;
+var EVENTS_ACTIVE = true;
 
 (function () {
   'use strict';
